12dc72b042e843b188f4171e2f7a184185490442
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-17 19:19:38 -0300

    new tests in challenge

--- functions/challenge_01.js
+++ functions/challenge_01.js
@@ -19,7 +19,25 @@ function calcular(a) {
     };
   };
 }
-const calculo = calcular(1)(2)((a = 0, b = 0) => {
+
+const fnSomar = (a = 0, b = 0) => {
   return a + b;
-});
-console.log("calculo=>", calculo);
+};
+const fnSubtrair = (a = 0, b = 0) => {
+  return a - b;
+};
+const fnDividir = (a = 0, b = 1) => {
+  return a / b;
+};
+const fnMultiplicar = (a = 0, b = 0) => {
+  return a * b;
+};
+const calculoSomar = calcular(1)(2)(fnSomar);
+const calculosubtrair = calcular(10)(2)(fnSubtrair);
+const calculoDividir = calcular(10)(2)(fnDividir);
+const calculoMultiplicar = calcular(10)(2)(fnMultiplicar);
+
+console.log("calculoSomar =>", calculoSomar);
+console.log("calculosubtrair =>", calculosubtrair);
+console.log("calculoDividir =>", calculoDividir);
+console.log("calculoMultiplicar =>", calculoMultiplicar);
