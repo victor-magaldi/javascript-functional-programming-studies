bbb65005d8eef8c5060c6ce925cc0b2c2dc14c82
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-01-30 20:40:25 -0300

    refatoring challenge

--- functions/callback_01.js
+++ functions/callback_01.js
@@ -1,9 +1,10 @@
 function exec(fn, a, b) {
-  console.log(fn(a, b));
+  return fn(a, b);
 }
 
 const somarTerminal = (a, b) => a + b;
 const subrtrairTerminal = (a, b) => a - b;
 
-exec(somarTerminal, 36, 38);
-exec(subrtrairTerminal, 36, 38);
+const soma = exec(somarTerminal, 36, 38);
+const subtracao = exec(subrtrairTerminal, 36, 38);
+console.log("soma", soma, "subtracao", subtracao);
