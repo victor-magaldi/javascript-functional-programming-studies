751007772fe75b0e5832b605b564d24027caf4cc
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-02-04 19:31:42 -0300

    init maps

---
+++ functions/map_01.js
@@ -0,0 +1,7 @@
+const num = [1, 2, 3, 4, 5, 6];
+const double = num.map((num) => num * 2);
+console.log("double", double);
+
+const names = ["Ana", "Bia", "Carlos", "David", "Esther"];
+const firstLetter = names.map((name) => name[0]);
+console.log("firstLetter", firstLetter);
