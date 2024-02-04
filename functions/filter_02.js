369140d3949874edf69582674094574cf252c6e6
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-02-04 20:04:06 -0300

    create Filter custom

--- functions/filter_02.js
+++ functions/filter_02.js
@@ -1,3 +1,13 @@
+Array.prototype.myFilterCustom = function (fn) {
+  let arr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (fn(this[i])) {
+      arr.push(this[i]);
+    }
+  }
+  return arr;
+};
+
 const cart = [
   { name: "Caderno", qtd: 5, price: 15.5 },
   { name: "Mochila", qtd: 1, price: 68.0 },
@@ -5,9 +15,8 @@ const cart = [
   { name: "Estojo", qtd: 0, price: 1.99 },
   { name: "Borracha", qtd: 0, price: 1.99 },
 ];
-
-const qtdGreaThanZero = cart.filter((item) => item.qtd > 0);
-const priceGreaThan10 = cart.filter((item) => item.price > 10);
+const qtdGreaThanZero = cart.myFilterCustom((item) => item.qtd > 0);
+const priceGreaThan10 = cart.myFilterCustom((item) => item.price > 10);
 
 console.log("qtdGreaThanZero", qtdGreaThanZero);
 console.log("priceGreaThan10", priceGreaThan10);
