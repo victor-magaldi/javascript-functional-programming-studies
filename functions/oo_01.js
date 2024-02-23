7a83c496b1af310604f9b36fe199fae0e800c649
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-02-23 19:09:32 -0300

    Objects in Functions

---
+++ functions/oo_01.js
@@ -0,0 +1,16 @@
+function Produto(name, price, desc = 0.15) {
+  this.name = name;
+  this.price = price;
+  this.desc = desc;
+
+  this.sellingPrice = () => this.price * (1 - desc);
+}
+
+const p1 = new Produto("Geladeira", 2000);
+const p2 = new Produto("Caneta", 10, 0.1);
+
+console.log(p1);
+console.log(`Preço final da ${p1.name}:`, p1.sellingPrice());
+
+console.log(p2);
+console.log(`Preço final da ${p2.name}:`, p2.sellingPrice());
