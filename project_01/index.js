a080030035791ab8fa383ee5f02ff9d3e8c2f494
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2024-03-04 19:34:52 -0300

    array string

--- project_01/index.js
+++ project_01/index.js
@@ -6,4 +6,6 @@ const pathComplete = path.join(__dirname, "legendas");
 fn.readDirectory(pathComplete)
   .then((files) => fn.filterFilesByExtension(files, "srt"))
   .then((filesSrt) => fn.readFiles(filesSrt))
+  .then((content) => content.join(""))
+  .then((allContent) => allContent.split("\n").length)
   .then(console.log);
